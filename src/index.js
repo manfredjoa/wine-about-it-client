ca6386af0bf51d41a6a0b13a29b298dc2f008284
manfredjoa <joamanfred@gmail.com>
2023-08-09 17:22:24 -0400

    remove bootstrap, add material tailwinds, create winecarousel component

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./tailwind.css";
+import { ThemeProvider } from "@material-tailwind/react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <App />
+      </Router>
+    </ThemeProvider>
   </React.StrictMode>
 );
