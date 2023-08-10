e0000670e247e0ab051e07467c314f224fa5138a
manfredjoa <joamanfred@gmail.com>
2023-08-09 22:23:15 -0400

    VERY minor change made directly on development branch; removed "image" from alt text to resolve terminal redundancy lint warning

--- src/components/WineCarousel.jsx
+++ src/components/WineCarousel.jsx
@@ -20,17 +20,17 @@ export default function WineCarousel() {
     >
       <img
         src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-        alt="image 1"
+        alt="1"
         className="h-screen w-screen object-cover"
       />
       <img
         src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-        alt="image 2"
+        alt="2"
         className="h-screen w-screen object-cover"
       />
       <img
         src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
-        alt="image 3"
+        alt="3"
         className="h-screen w-screen object-cover"
       />
     </Carousel>
