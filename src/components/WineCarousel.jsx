a7a56f92c7422492829b145b17b608dcac585ef2
manfredjoa <joamanfred@gmail.com>
2023-08-11 19:49:31 -0400

    change wine detail button from see more to add to cart

--- src/components/WineCarousel.jsx
+++ src/components/WineCarousel.jsx
@@ -33,6 +33,21 @@ export default function WineCarousel() {
         alt="3"
         className="h-screen w-screen object-cover"
       />
+      <img
+        src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
+        alt="1"
+        className="h-screen w-screen object-cover"
+      />
+      <img
+        src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+        alt="2"
+        className="h-screen w-screen object-cover"
+      />
+      <img
+        src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
+        alt="3"
+        className="h-screen w-screen object-cover"
+      />
     </Carousel>
   );
 }
--- src/components/WineDetail.jsx
+++ src/components/WineDetail.jsx
@@ -1,20 +1,4 @@
 import { Button } from "@material-tailwind/react";
-import React from "react";
-
-// const mockData = {
-//   _id: "64d404d5962ba4705fca0ecf",
-//   WineName: "Dominus 2014",
-//   Price: 229.97,
-//   CountryState: "California",
-//   Region: "Napa Valley",
-//   ProductType: "Red Wine",
-//   VarietalType: "Cabernet Sauvignon",
-//   Description:
-//     "Wine Advocate-Napa CA - The 2014 is a sexy opulent layered multidimensional wine with notes of red and blackcurrant cedar wood tobacco leaf spice box and licorice. The color is a dense plum/ruby/purple much like the Napanook. This is a beauty that is already drinking gorgeously... ()",
-//   img: "https://images.vivino.com/thumbs/FvLdztxrRXOZxU1jfVoQKQ_pb_x960.png",
-//   flag: "https://flagsapi.com/US/flat/64.png",
-//   __v: 0,
-// };
 
 function WineDetail({ wine }) {
   return (
@@ -27,7 +11,7 @@ function WineDetail({ wine }) {
             <p className="font-bold text-2xl">{wine.WineName}</p>
           </div>
           <div>
-            <Button>See More</Button>
+            <Button>Add to Cart</Button>
           </div>
         </div>
       </div>
