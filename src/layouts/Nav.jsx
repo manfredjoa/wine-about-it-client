3728f410009a2f3941bbc7e456aef1401cd84db0
manfredjoa <joamanfred@gmail.com>
2023-08-12 09:45:23 -0400

    create layout component and wrap all pages in it

--- src/layouts/Nav.jsx
+++ src/layouts/Nav.jsx
@@ -1,24 +1,19 @@
-import React from 'react';
-
-const Nav = () => {
+export default function Nav() {
   return (
     <header className="header">
       <div className="header-content">
         <p> Wine About It</p>
-        <ul className='header-links'></ul>
-        <li className="User icon"><a href="/usericon"> UserIcon </a> </li>
-        <li className="Cart"><a href="/cart"> Cart</a></li>
-        <li className='Search bar'><a href='/search'>Search</a></li>
-
-
+        <ul className="header-links"></ul>
+        <li className="User icon">
+          <a href="/usericon"> UserIcon </a>{" "}
+        </li>
+        <li className="Cart">
+          <a href="/cart"> Cart</a>
+        </li>
+        <li className="Search bar">
+          <a href="/search">Search</a>
+        </li>
       </div>
     </header>
-
-
-
-
-  )
+  );
 }
-
-
-export default Nav;
